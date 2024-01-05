e89abb81f57094b728a41f56aad9cb395116a593
Felipe Salomao <lipesalomaoclaf@gmail.com>
2024-01-05 15:08:12 -0300

    fix: eslint

--- api/views.py
+++ api/views.py
@@ -2,45 +2,44 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.models import Card
 from api.serializers import CardSerializer
-import random
 from api.utils import random_card
 
 
 
 @api_view(['GET'])
 def get_cards(request):
-    cards = Card.objects.all()
+  cards = Card.objects.all()
 
-    Card.objects.filter(chosen=True).update(chosen=False)
+  Card.objects.filter(chosen=True).update(chosen=False)
 
-    random_cards = random_card(cards)
-    random_cards.save()
+  random_cards = random_card(cards)
+  random_cards.save()
 
-    serializer = CardSerializer(cards, many=True)
+  serializer = CardSerializer(cards, many=True)
     
-    return Response(serializer.data)
+  return Response(serializer.data)
 
 @api_view(['POST'])
 def create_card(request):
-    serializer = CardSerializer(data=request.data)
-    if serializer.is_valid():      
-      serializer.save()
-      return Response(serializer.data)
-    
-    return Response(serializer.errors)
+  serializer = CardSerializer(data=request.data)
+  if serializer.is_valid():      
+    serializer.save()
+    return Response(serializer.data)
+  
+  return Response(serializer.errors)
 
 @api_view(['GET'])
 def select_card(request, card_id):
-    try:
-        card = Card.objects.get(id=card_id)
-        serializer = CardSerializer(card).data
-        
-        if not card.chosen:
-          return Response({"error": "wrong card"}, status=404)
+  try:
+    card = Card.objects.get(id=card_id)
+    serializer = CardSerializer(card).data
+      
+    if not card.chosen:
+      return Response({"error": "wrong card"}, status=404)
 
-        return Response(serializer)
-    except Card.DoesNotExist:
-        return Response({"error": "Card not found"}, status=404)
+    return Response(serializer)
+  except Card.DoesNotExist:
+    return Response({"error": "Card not found"}, status=404)
 
   
 
